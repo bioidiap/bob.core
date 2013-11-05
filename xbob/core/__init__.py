235f7855451dd27d91f1bbc94c60d6854411a1f1
Andre Anjos <andre.dos.anjos@gmail.com>
2013-11-05 09:21:20 +0100

    Adds get_include

--- xbob/core/__init__.py
+++ xbob/core/__init__.py
@@ -1,2 +1,10 @@
 from ._convert import convert
 from . import log
+from . import random
+
+def get_include():
+  """Returns the directory containing the C/C++ API include directives"""
+
+  return __import__('pkg_resources').resource_filename(__name__, 'include')
+
+__all__ = ['convert', 'log', 'random']
