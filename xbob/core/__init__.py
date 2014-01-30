919dddc1c0dd9afddfff3c868bd4766a9f57cbf1
Andre Anjos <andre.dos.anjos@gmail.com>
2014-01-30 08:51:05 +0100

    Remove __all__ specifier

--- xbob/core/__init__.py
+++ xbob/core/__init__.py
@@ -7,5 +7,3 @@ def get_include():
   """Returns the directory containing the C/C++ API include directives"""
 
   return __import__('pkg_resources').resource_filename(__name__, 'include')
-
-__all__ = ['convert', 'log', 'random']
