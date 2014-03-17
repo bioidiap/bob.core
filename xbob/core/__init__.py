1d46493949e9e966dd6684de7e1d83faa1f7e86e
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-17 09:14:29 +0100

    Fix python3 compatibility issues with doc fix

--- xbob/core/__init__.py
+++ xbob/core/__init__.py
@@ -7,3 +7,6 @@ def get_include():
   """Returns the directory containing the C/C++ API include directives"""
 
   return __import__('pkg_resources').resource_filename(__name__, 'include')
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
