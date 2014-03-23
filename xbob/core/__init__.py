5747284df86016958ab1ae9dcf437b375e79beba
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-23 08:22:53 +0100

    Add function to print comprehensive dependence list

--- xbob/core/__init__.py
+++ xbob/core/__init__.py
@@ -10,5 +10,24 @@ def get_include():
 
   return __import__('pkg_resources').resource_filename(__name__, 'include')
 
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+
+  import pkg_resources
+  from .version import externals
+
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - c/c++ dependencies:\n"
+  for k in sorted(externals): retval += "    - %s: %s\n" % (k, externals[k])
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
