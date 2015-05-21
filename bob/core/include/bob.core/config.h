47c4b39c89db898fce916344fbbe3e1b57e4f84e
Manuel Guenther <manuel.guenther@idiap.ch>
2015-05-21 13:26:01 +0200

    Included funcitonality for version handling in config.h; use new bob.blitz api for versioning

--- bob/core/include/bob.core/config.h
+++ bob/core/include/bob.core/config.h
@@ -1,6 +1,6 @@
 /**
  * @author Andre Anjos <andre.anjos@idiap.ch>
- * @date Fri  1 Nov 07:10:59 2013 
+ * @date Fri  1 Nov 07:10:59 2013
  *
  * @brief General directives for all modules in bob.core
  */
@@ -10,5 +10,22 @@
 
 /* Macros that define versions and important names */
 #define BOB_CORE_API_VERSION 0x0200
+
+#ifdef BOB_IMPORT_VERSION
+
+  /***************************************
+  * Here we define some functions that should be used to build version dictionaries in the version.cpp file
+  * There will be a compiler warning, when these functions are not used, so use them!
+  ***************************************/
+
+
+  /**
+   * bob.core c/c++ api version
+   */
+  static PyObject* bob_core_version() {
+    return Py_BuildValue("{ss}", "api", BOOST_PP_STRINGIZE(BOB_CORE_API_VERSION));
+  }
+
+#endif // BOB_IMPORT_VERSION
 
 #endif /* BOB_CORE_CONFIG_H */
